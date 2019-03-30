ea080d0ecc4ec32cd39ad2405c9c48cf4a98427c
mdsaadlaoui <mdsaadlaoui@TNLT649.vermeg.com>
2019-03-30 02:24:11 +0100

    DAO LAYER DONE

---
+++ src/tn/esprit/dao/implementation/AbonnementDaoImpl.java
@@ -0,0 +1,211 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tn.esprit.dao.implementation;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import tn.esprit.dao.exceptions.DataBaseException;
+import tn.esprit.dao.interfaces.IAbonnementDao;
+import tn.esprit.dao.interfaces.ICompanyDao;
+import tn.esprit.dao.interfaces.IUserDao;
+import tn.esprit.entities.Abonnement;
+import tn.esprit.entities.Company;
+import tn.esprit.entities.User;
+
+/**
+ *
+ * @author Alai Zid
+ */
+public final class AbonnementDaoImpl extends GenericDaoImpl implements IAbonnementDao {
+
+    private final ICompanyDao companyDao;
+    private final IUserDao userDao;
+
+    /**
+     *
+     */
+    public AbonnementDaoImpl() {
+        companyDao = new CompanyDaoImpl();
+        userDao = new UserDaoImpl();
+    }
+
+    /**
+     *
+     * @param candidate
+     * @return
+     * @throws DataBaseException
+     */
+    @Override
+    public List<Abonnement> findByCandidate(User candidate) throws DataBaseException {
+
+        List<Abonnement> list = new ArrayList<>();
+        selectQuery = queriesFactory.newSelectQuery();
+        selectQuery
+                .select(queriesFactory.newAllField())
+                .from("abonnement")
+                .where()
+                .where(queriesFactory.newStdField("candidate"), ":candidate");
+        try {
+            preparedStatement = cnx.prepareStatement(selectQuery.getQueryString());
+            preparedStatement.setInt(selectQuery.getPlaceholderIndex(":candidate"), candidate.getId());
+            resultSet = preparedStatement.executeQuery();
+            while (resultSet.next()) {
+                list.add(new Abonnement.Builder()
+                        .candidate(userDao.findByID(resultSet.getInt("candidate")))
+                        .company(companyDao.findByRecruter(new User.Builder().id(resultSet.getInt("company")).build()))
+                        .dateAbonnement(resultSet.getDate("dateabonnement"))
+                        .build());
+
+            }
+
+        } catch (SQLException ex) {
+            throw new DataBaseException(ex.getMessage());
+        }
+
+        return list;
+    }
+
+    /**
+     *
+     * @param company
+     * @return
+     * @throws DataBaseException
+     */
+    @Override
+    public List<Abonnement> findByCompany(Company company) throws DataBaseException {
+
+        List<Abonnement> list = new ArrayList<>();
+        selectQuery = queriesFactory.newSelectQuery();
+        selectQuery
+                .select(queriesFactory.newAllField())
+                .from("abonnement")
+                .where()
+                .where(queriesFactory.newStdField("company"), ":company");
+        try {
+            preparedStatement = cnx.prepareStatement(selectQuery.getQueryString());
+            preparedStatement.setInt(selectQuery.getPlaceholderIndex(":company"), company.getRecruiter().getId());
+            resultSet = preparedStatement.executeQuery();
+            while (resultSet.next()) {
+                list.add(new Abonnement.Builder()
+                        .candidate(userDao.findByID(resultSet.getInt("candidate")))
+                        .company(companyDao.findByRecruter(new User.Builder().id(resultSet.getInt("company")).build()))
+                        .dateAbonnement(resultSet.getDate("dateabonnement"))
+                        .build());
+
+            }
+
+        } catch (SQLException ex) {
+            throw new DataBaseException(ex.getMessage());
+        }
+
+        return list;
+
+    }
+
+    @Override
+    public List<Abonnement> findAll() throws DataBaseException {
+        List<Abonnement> list = new ArrayList<>();
+        selectQuery = queriesFactory.newSelectQuery();
+        selectQuery
+                .select(queriesFactory.newAllField())
+                .from("abonnement");
+        try {
+
+            resultSet = cnx.getResult(selectQuery.getQueryString());
+            while (resultSet.next()) {
+                list.add(new Abonnement.Builder()
+                        .candidate(userDao.findByID(resultSet.getInt("candidate")))
+                        .company(companyDao.findByRecruter(new User.Builder().id(resultSet.getInt("company")).build()))
+                        .dateAbonnement(resultSet.getDate("dateabonnement"))
+                        .build());
+
+            }
+
+        } catch (SQLException ex) {
+            throw new DataBaseException(ex.getMessage());
+        }
+
+        return list;
+    }
+
+    @Override
+    public Integer create(Abonnement entity) throws DataBaseException {
+
+        Integer rowsCreated = 0;
+        insertQuery = queriesFactory.newInsertQuery();
+        insertQuery.set(queriesFactory.newStdField("candidate"), ":candidate")
+                .set(queriesFactory.newStdField("company"), ":company")
+                .set(queriesFactory.newStdField("dateabonnement"), ":dateabonnement")
+                .inTable("abonnement");
+
+        try {
+            preparedStatement = cnx.prepareStatement(insertQuery.getQueryString());
+            preparedStatement.setObject(insertQuery.getPlaceholderIndex(":candidate"), entity.getCandidate() != null ? entity.getCandidate().getId() : null, java.sql.Types.INTEGER);
+            preparedStatement.setObject(insertQuery.getPlaceholderIndex(":company"), entity.getCompany() != null ? entity.getCompany().getRecruiter().getId() : null, java.sql.Types.INTEGER);
+            preparedStatement.setDate(insertQuery.getPlaceholderIndex(":dateabonnement"), new java.sql.Date(entity.getDateAbonnement().getTime()));
+            rowsCreated = preparedStatement.executeUpdate();
+
+        } catch (SQLException ex) {
+            throw new DataBaseException(ex.getMessage());
+        }
+
+        return rowsCreated;
+
+    }
+
+    @Override
+    public Integer edit(Abonnement entity) throws DataBaseException {
+        Integer rowUpdated = 0;
+        updateQuery = queriesFactory.newUpdateQuery();
+        updateQuery
+                .set(queriesFactory.newStdField("candidate"), ":candidate")
+                .set(queriesFactory.newStdField("company"), ":company")
+                .set(queriesFactory.newStdField("dateabonnement"), ":dateabonnement")
+                .inTable("abonnement")
+                .where()
+                .where(queriesFactory.newStdField("candidate"), ":candidateid")
+                .where(queriesFactory.newStdField("company"), ":companyid");
+        try {
+            preparedStatement = cnx.prepareStatement(updateQuery.getQueryString());
+            preparedStatement.setObject(updateQuery.getPlaceholderIndex(":candidate"), entity.getCandidate() != null ? entity.getCandidate().getId() : null, java.sql.Types.INTEGER);
+            preparedStatement.setObject(updateQuery.getPlaceholderIndex(":company"), entity.getCompany() != null ? entity.getCompany().getRecruiter().getId() : null, java.sql.Types.INTEGER);
+            preparedStatement.setDate(updateQuery.getPlaceholderIndex(":dateabonnement"), new java.sql.Date(entity.getDateAbonnement().getTime()));
+            preparedStatement.setObject(updateQuery.getPlaceholderIndex(":candidateid"), entity.getCandidate() != null ? entity.getCandidate().getId() : null, java.sql.Types.INTEGER);
+            preparedStatement.setObject(updateQuery.getPlaceholderIndex(":companyid"), entity.getCompany() != null ? entity.getCompany().getRecruiter().getId() : null, java.sql.Types.INTEGER);
+
+            rowUpdated = preparedStatement.executeUpdate();
+        } catch (SQLException ex) {
+            throw new DataBaseException(ex.getMessage());
+        }
+
+        return rowUpdated;
+    }
+
+    @Override
+    public Integer delete(Abonnement entity) throws DataBaseException {
+
+        Integer rowDeleted = 1;
+        deleteQuery = queriesFactory.newDeleteQuery();
+        deleteQuery.from("abonnement")
+                .where()
+                .where(queriesFactory.newStdField("candidate"), ":candidateid")
+                .where(queriesFactory.newStdField("company"), ":companyid");
+
+        try {
+            preparedStatement = cnx.prepareStatement(deleteQuery.getQueryString());
+            preparedStatement.setObject(deleteQuery.getPlaceholderIndex(":candidateid"), entity.getCandidate() != null ? entity.getCandidate().getId() : null, java.sql.Types.INTEGER);
+            preparedStatement.setObject(deleteQuery.getPlaceholderIndex(":companyid"), entity.getCompany() != null ? entity.getCompany().getRecruiter().getId() : null, java.sql.Types.INTEGER);
+
+            rowDeleted = preparedStatement.executeUpdate();
+        } catch (SQLException ex) {
+            throw new DataBaseException(ex.getMessage());
+        }
+
+        return rowDeleted;
+    }
+
+}
--- src/tn/esprit/unit/test/business/TestHabib.java
+++ src/tn/esprit/unit/test/business/TestHabib.java
@@ -4,23 +4,29 @@
  * and open the template in the editor.
  */
 package tn.esprit.unit.test.business;
+
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import tn.esprit.dao.exceptions.DataBaseException;
+import tn.esprit.dao.implementation.AbonnementDaoImpl;
 import tn.esprit.dao.implementation.ApplyDaoImpl;
 import tn.esprit.dao.implementation.CategoryDaoImpl;
 import tn.esprit.dao.implementation.CommentDaoImpl;
 import tn.esprit.dao.implementation.CompanyDaoImpl;
 import tn.esprit.dao.implementation.JobDaoImpl;
+import tn.esprit.dao.implementation.RateDaoImpl;
 import tn.esprit.dao.implementation.ReclamationDaoImpl;
 import tn.esprit.dao.implementation.UserDaoImpl;
+import tn.esprit.dao.interfaces.IAbonnementDao;
 import tn.esprit.dao.interfaces.IApplyDao;
 import tn.esprit.dao.interfaces.ICategoryDao;
 import tn.esprit.dao.interfaces.ICommentDao;
 import tn.esprit.dao.interfaces.ICompanyDao;
 import tn.esprit.dao.interfaces.IJobDao;
+import tn.esprit.dao.interfaces.IRateDao;
 import tn.esprit.dao.interfaces.IReclamationDao;
 import tn.esprit.dao.interfaces.IUserDao;
+import tn.esprit.entities.Rate;
 import tn.esprit.services.implementation.ServiceUserImpl;
 import tn.esprit.services.interfaces.IServiceUser;
 
@@ -44,14 +50,15 @@ public class TestHabib {
         ICommentDao commentDao = new CommentDaoImpl();
         IServiceUser serviceUser = new ServiceUserImpl();
         IReclamationDao reclamationdao = new ReclamationDaoImpl();
+        IAbonnementDao abonnementDao = new AbonnementDaoImpl();
+        IRateDao rateDao = new RateDaoImpl();
 
         try {
-            System.out.println(reclamationdao.findAll());
-            System.out.println(reclamationdao.findById(7));
-            System.out.println(reclamationdao.delete(reclamationdao.findById(9)));
+            rateDao.delete(rateDao.findAll().get(0));
         } catch (DataBaseException ex) {
-            Logger.getLogger(TestHabib.class.getName()).log(Level.SEVERE, null, ex);
+            ex.getMessage();
         }
 
     }
+
 }
