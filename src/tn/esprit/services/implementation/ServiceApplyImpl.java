6b9b9ac9a39522a3382cd3e3b21dd5ec25693ada
mourad <mourad@DESKTOP-9F52TAU>
2019-04-04 11:25:58 +0200

    added service implementation for Apply and a methode advancedsearch
    this methode search for an apply by the job name (example developper) and the category example (java developper) and the minimum salary and max salary this methode will be called to advance search for an apply where we will get texts from a formula and pass it the the methode parametres

---
+++ src/tn/esprit/services/implementation/ServiceApplyImpl.java
@@ -0,0 +1,72 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tn.esprit.services.implementation;
+
+import java.util.ArrayList;
+import java.util.List;
+import tn.esprit.dao.exceptions.DataBaseException;
+import tn.esprit.dao.implementation.ApplyDaoImpl;
+import tn.esprit.dao.interfaces.IApplyDao;
+import tn.esprit.entities.Apply;
+import tn.esprit.entities.Job;
+import tn.esprit.entities.User;
+import tn.esprit.services.interfaces.IServiceApply;
+
+/**
+ *
+ * @author mourad
+ */
+public class ServiceApplyImpl implements IServiceApply{
+    
+    IApplyDao iapply;
+    
+    public ServiceApplyImpl(){
+        iapply = new ApplyDaoImpl();
+    }
+    
+    
+     @Override
+    public List<Apply> advancedsearch(String job, String category, double salarymin, double salarymax) throws DataBaseException {
+        List<Apply> l = iapply.findAll();
+         List<Apply> advancedapply = new ArrayList<>();
+         l.stream().filter((a) -> ((a.getJob().getCategory().getLabel().equals(category))&& (a.getJob().getTitle().equals(job))&&((a.getJob().getSalary() >= salarymin))&&(a.getJob().getSalary() <= salarymax))).forEachOrdered((a) -> {
+             advancedapply.add(a);
+        });
+    return advancedapply;
+}
+
+    @Override
+    public List<Apply> findAll() throws DataBaseException {
+        return iapply.findAll();
+    }
+
+    @Override
+    public Integer create(Apply entity) throws DataBaseException {
+        return iapply.create(entity);
+    }
+
+    @Override
+    public Integer edit(Apply entity) throws DataBaseException {
+        return iapply.create(entity);
+    }
+
+    @Override
+    public Integer delete(Apply entity) throws DataBaseException {
+        return iapply.delete(entity);
+    }
+
+    @Override
+    public List<Apply> findByjob(Job job) throws DataBaseException {
+       return iapply.findByjob(job);
+    }
+
+    @Override
+    public List<Apply> findBycandidate(User candidate) throws DataBaseException {
+        return iapply.findBycandidate(candidate);
+    }
+
+   
+}
